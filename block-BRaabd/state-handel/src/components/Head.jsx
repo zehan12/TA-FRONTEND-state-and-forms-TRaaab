8e38a92f9ab5165df55e437bed4652b96b4799ff
Zehan Khan <zehan9211@gmail.com>
2022-06-01 13:28:49 +0530

    add Hooks useState

--- block-BRaabd/state-handel/src/components/Head.jsx
+++ block-BRaabd/state-handel/src/components/Head.jsx
@@ -1,28 +1,41 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from 'react';
 
-const text = [ "basketball", "cricket", "laptop", "phone", "pubg", "tiger" ]
 
-class Head extends React.Component {
-    constructor ( props ) {
-    super( props );
-        this.state = {
-            imgUrl: "/assets/show-image.png"
-        }
-    }
- 
-    render (  ){
-    return ( <>
-        {    console.log(this.state.imgUrl) }
-        { text.map((ele,i)=><button onClick={ ( event ) => {
-            let val = event.target.innerText
-            this.setState({
-                imgUrl: `/assets/${val}.jpg`
-            })
-        } } key={i} > {ele} </button> ) }
+const text = ["basketball", "cricket", "laptop", "phone", "pubg", "tiger"]
+
+// class Head extends React.Component {
+//     constructor ( props ) {
+//     super( props );
+//         this.state = {
+//             imgUrl: "show-image"
+//         }
+//     }
+
+//     render (  ){
+//     return ( <>
+//         { text.map((ele,i)=><button className={ this.state.imgUrl == ele ? "active" : "" } onClick={ ( event ) => {
+//             this.setState({
+//                 imgUrl: event.target.innerText
+//             })
+//         } } key={i} > {ele} </button> ) }
+//         <br></br>
+//         <img src={"/assets/"+this.state.imgUrl+".jpg"} alt="" width="400px" height="300px" />
+//     </> )
+//     }
+// }
+
+function Head() {
+    const [imgUrl, setImgUrl] = useState("show-image");
+    console.warn("page render threw state")
+    return (<>
+        {text.map((ele, i) => <button className={ imgUrl == ele ? "active" : "" }  onClick={(event) => {
+            setImgUrl(event.target.innerText)
+            }
+        } key={i} > {ele} </button>)}
         <br></br>
-        <img src={this.state.imgUrl} alt="" width="400px" height="300px" />
-    </> )
-    }
+        <img src={"/assets/" + imgUrl + ".jpg"} alt="" width="400px" height="300px" />
+    </>)
 }
 
 export default Head;
