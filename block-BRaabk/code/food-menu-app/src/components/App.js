90c3c02974bdab0a98cc0d09ab88c779bc015e8a
Zehan Khan <zehan9211@gmail.com>
2022-06-04 16:48:04 +0530

    food menu

---
+++ block-BRaabk/code/food-menu-app/src/components/App.js
@@ -0,0 +1,14 @@
+import '../index.css';
+import Header from './header';
+import Tags from './Tags';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Tags />
+    </div>
+  );
+}
+
+export default App;
