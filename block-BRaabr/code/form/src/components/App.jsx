598ef5476cbe5f7466668a2f2f4e796457386984
Zehan Khan <zehan9211@gmail.com>
2022-06-09 18:15:26 +0530

    control

---
+++ block-BRaabr/code/form/src/components/App.jsx
@@ -0,0 +1,13 @@
+import Multiform from "./Multiform";
+import Validateform from "./Validateform";
+
+function App () {
+    return (
+        <>
+            <Multiform />
+            <Validateform />
+        </>
+    )
+}
+
+export default App;
