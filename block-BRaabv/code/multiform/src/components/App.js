be9942a180bd2f262bcc6db5d2ff23473405b992
Zehan Khan <zehan9211@gmail.com>
2022-06-12 22:06:12 +0530

    push code

---
+++ block-BRaabv/code/multiform/src/components/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor( props ){
+    super();
+    this.state = {};
+  }
+
+  render(){
+    return (
+        <>
+        <div className="border bg-wihte">
+          <h1>hello</h1>
+        </div>
+        </>
+    );
+  }
+}
+
+class SignUp extends React.Component {
+  render(){
+    return
+  }
+}
+
+class Message extends React.Component {
+  render(){
+    return
+  }
+}
+
+class Checkbox extends React.Component {
+  render(){
+    return
+  }
+}
+
+export default App;
